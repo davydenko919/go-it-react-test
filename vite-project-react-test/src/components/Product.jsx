33674e9a049f69a72bcf325a8ddfb2877c8912fb
davydenko919 <davydenko919@gmail.com>
2024-04-13 14:54:06 +0300

    s1

---
+++ vite-project-react-test/src/components/Product.jsx
@@ -0,0 +1,13 @@
+// src/Product.jsx
+
+const Product = () => {
+      return (
+        <div>
+          <h2>Tacos</h2>
+                <p>Price: 999 credits</p>
+        </div>
+      );
+    };
+    
+    export default Product;
+    
