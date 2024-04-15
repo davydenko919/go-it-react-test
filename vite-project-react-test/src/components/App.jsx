b319425ca25a07034c25c07e64c7fa9f49bb68b5
davydenko919 <davydenko919@gmail.com>
2024-04-15 14:07:35 +0300

    s1

--- vite-project-react-test/src/components/App.jsx
+++ vite-project-react-test/src/components/App.jsx
@@ -1,16 +1,31 @@
 // src/App.jsx
 
-import { Product } from './Product';
+import { BookList } from './Product';
+
+
+
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+
+// export default function App = () => {
+//   return (
+//     <>
+// 			<h1>Books of the week</h1>
+//       <BookList books={favouriteBooks} />
+//     </>
+//   );
+// };
 
 
 export default function App() {
-  return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product />
-      <Product />
-      <Product />
-    </div>
-  );
+    return (
+      <>
+        <h1>Books of the week</h1>
+        <BookList books={favouriteBooks} />
+      </>
+    );
 }
--- vite-project-react-test/src/components/Product.jsx
+++ vite-project-react-test/src/components/Product.jsx
@@ -1,13 +1,24 @@
 // src/components/Product.jsx
 
-export const Product = () => {
-	const price = 999;
+// export const Product = () => {
+// 	const price = 999;
 
+//   return (
+//     <>
+//       <h2>Tacos</h2>
+// 			<img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
+//       <p>Price: {price} credits</p>
+//     </>
+//   );
+// };
+
+
+export const BookList = ({ books }) => {
   return (
-    <>
-      <h2>Tacos</h2>
-			<img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
+    <ul>
+      {books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      })}
+    </ul>
   );
 };
